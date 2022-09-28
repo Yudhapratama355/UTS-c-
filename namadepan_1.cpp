6ecc32245e286f563e41303546aec5bc8a908481
Yudhapratama355 <111962863+Yudhapratama355@users.noreply.github.com>
2022-09-28 15:30:46 +0700

    Add files via upload

---
+++ namadepan_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nama:Yudha pratama"<<endl;
+    cout << "Kelas:XRPL1"<<endl;
+    cout << "Nilai:95"<<endl;
+    
+}
---
+++ rumusbangundatar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int luas, keliling;
+  int r,d;
+  const float phi = 3.14;
+  cout << "aplikasi hitung luas+keliling lingkaran"<<endl;
+  cout << "masukkan jari jari lingkaran ="; cin >> r;
+  luas = phi*r*r;
+  keliling = 2*phi*r;
+  cout << "luas lingkaran ="<<luas<<endl;
+  cout << "keliling lingkaran ="<<keliling<<endl;
+    
+}
